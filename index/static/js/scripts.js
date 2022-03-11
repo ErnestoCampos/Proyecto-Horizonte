20e1109075697f6941cdfd628572fffbc5612f5e
Ernesto Campos <camposernesto2004@gmail.com>
2022-03-11 17:18:29 -0300

    sexto commit

--- index/static/js/scripts.js
+++ index/static/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Full Width Pics v5.0.4 (https://startbootstrap.com/template/full-width-pics)
+* Start Bootstrap - Landing Page v6.0.4 (https://startbootstrap.com/theme/landing-page)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
