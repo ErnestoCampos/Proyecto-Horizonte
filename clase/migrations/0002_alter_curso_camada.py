b93e0638273a516a2cab5e897f93e9c47137d7e9
Ernesto Campos <camposernesto2004@gmail.com>
2022-03-11 15:45:50 -0300

    "quinto commit"

--- clase/migrations/0002_alter_curso_camada.py
+++ clase/migrations/0002_alter_curso_camada.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('class', '0001_initial'),
+        ('clase', '0001_initial'),
     ]
 
     operations = [
